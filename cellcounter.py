df7caa2487aa600d4b5cdac40ba294914e425720
Blake Richards <tyrell.turing@gmail.com>
2013-03-07 14:30:01 -0500

    added basic cell counting to cellcounter.py

--- cellcounter.py
+++ cellcounter.py
@@ -7,6 +7,7 @@ import pymorph
 import mahotas
 from scipy import ndimage
 from libtiff import TIFF, TIFFfile
+import pdb
 
 ###############################################################################
 class Image:
@@ -22,9 +23,10 @@ class Image:
 	def __init__(self, filename, **kwargs):
 
 		# parse the kwargs
-		self.colours  = kwargs.get('colours',('b','g','r'))  # colour of each channel
-		self.rgb      = kwargs.get('rgb',False)              # whether to treat the image as an rgb
-		self.zres     = kwargs.get('zres',1)                 # z resolution (in um/slice)
+		self.colours    = kwargs.get('colours',('b','g','r'))  # colour of each channel
+		self.rgb        = kwargs.get('rgb',False)              # whether to treat the image as an rgb
+		self.zres       = kwargs.get('zres',1)                 # z resolution (in um/slice)
+		self.findtissue = kwargs.get('findtissue',False)       # whether to get a tissue mask for the image
 
 		########################################################
 		### 1. OBTAIN INFO ABOUT THE IMAGE
@@ -98,25 +100,111 @@ class Image:
 			# load the original tif file
 			original = TIFFfile(self.filename)
 
-			# initialize the array holderis
-			self.array = np.empty([self.info['ImageWidth'],
-			                       self.info['ImageLength'],
-			                       self.info['slices'],
-			                       self.info['channels']],dtype=np.uint8)
+			# initialize the array holders
+			if 'slices' in self.info and 'channels' in self.info:
+				self.array = np.empty([self.info['ImageWidth'],
+															 self.info['ImageLength'],
+															 self.info['slices'],
+															 self.info['channels']],dtype=np.uint8)
+				self.imgtype = 'XYZC'
+			elif 'slices' in self.info:
+				self.array = np.empty([self.info['ImageWidth'],
+															 self.info['ImageLength'],
+															 self.info['slices']],dtype=np.uint8)
+				self.imgtype = 'XYZ'
+			elif 'channels' in self.info:
+				self.array = np.empty([self.info['ImageWidth'],
+															 self.info['ImageLength'],
+															 self.info['channels']],dtype=np.uint8)
+				self.imgtype = 'XYC'
+			else:
+				self.array = np.empty([self.info['ImageWidth'],
+															 self.info['ImageLength']],dtype=np.uint8)
+				self.imgtype = 'XY'
 
 			# get the raw array data and store it in the holder
-			samples = original.get_samples()[0][0]
-			for c in range(self.info['channels']):
+			if self.imgtype == 'XYZC':
+				samples = original.get_samples()[0][0]
+				for c in range(self.info['channels']):
 
-				# sort the colors if requested
-				if self.rgb and self.info['channels'] == 3:
-					if   self.colours[c] == 'b': ca = 2
-					elif self.colours[c] == 'g': ca = 1
-					elif self.colours[c] == 'r': ca = 0
-				else: ca = c
+					# sort the colors if requested
+					if self.rgb and self.info['channels'] == 3:
+						if   self.colours[c] == 'b': ca = 2
+						elif self.colours[c] == 'g': ca = 1
+						elif self.colours[c] == 'r': ca = 0
+					else: ca = c
 
+					for s in range(self.info['slices']):
+						self.array[:,:,s,ca] = np.uint8(samples[s*self.info['channels'] + c,:,:])
+
+			if self.imgtype == 'XYZ':
+				samples = original.get_samples()[0][0]
 				for s in range(self.info['slices']):
-					self.array[:,:,s,ca] = np.uint8(samples[s*self.info['channels'] + c,:,:])
+					self.array[:,:,s] = np.uint8(samples[s,:,:])
+
+			if self.imgtype == 'XYC':
+				samples = original.get_samples()[0][0]
+				for c in range(self.info['channels']):
+
+					# sort the colors if requested
+					if self.rgb and self.info['channels'] == 3:
+						if   self.colours[c] == 'b': ca = 2
+						elif self.colours[c] == 'g': ca = 1
+						elif self.colours[c] == 'r': ca = 0
+					else: ca = c
+
+					self.array[:,:,ca] = np.uint8(samples[c,:,:])
+
+			if self.imgtype == 'XY':
+				samples = original.get_samples()[0][0]
+				self.array[:,:] = np.uint8(samples[:,:])
+
+		########################################################
+	  ### 3. CREATE THE XYZ INFO
+
+		if self.imgtype == 'XYZC':
+			self.x = np.empty(self.array.shape)
+			self.y = np.empty(self.array.shape)
+			self.z = np.empty(self.array.shape)
+			self.c = np.empty(self.array.shape)
+			for c in range(self.info['channels']):
+				zstep = 0
+				self.c[:,:,:,c] = self.c
+				for s in range(self.info['slices']):
+					[self.x[:,:,s,c], self.y[:,:,s,c]] = np.meshgrid(np.arange(0,self.info['XResolution']*self.info['ImageWidth'],self.info['XResolution']),
+					                                                 np.arange(0,self.info['YResolution']*self.info['ImageLength'],self.info['YResolution']))
+					self.z[:,:,s,:] = zstep
+					zstep += self.zres
+
+		if self.imgtype == 'XYZ':
+			self.x = np.empty(self.array.shape)
+			self.y = np.empty(self.array.shape)
+			self.z = np.empty(self.array.shape)
+			zstep = 0
+			for s in range(self.info['slices']):
+				[self.x[:,:,s], self.y[:,:,s]] = np.meshgrid(np.arange(0,self.info['XResolution']*self.info['ImageWidth'],self.info['XResolution']),
+																										 np.arange(0,self.info['YResolution']*self.info['ImageLength'],self.info['YResolution']))
+				self.z[:,:,s] = zstep
+				zstep += self.zres
+
+		if self.imgtype == 'XYC':
+			self.x = np.empty(self.array.shape)
+			self.y = np.empty(self.array.shape)
+			self.c = np.empty(self.array.shape)
+			for c in range(self.info['channels']):
+				zstep = 0
+				self.c[:,:,c] = self.c
+				[self.x[:,:,c], self.y[:,:,c]] = np.meshgrid(np.arange(0,self.info['XResolution']*self.info['ImageWidth'],self.info['XResolution']),
+																										 np.arange(0,self.info['YResolution']*self.info['ImageLength'],self.info['YResolution']))
+
+		if self.imgtype == 'XY':
+			self.x = np.empty(self.array.shape)
+			self.y = np.empty(self.array.shape)
+			[self.x[:,:], self.y[:,:]] = np.meshgrid(np.arange(0,self.info['XResolution']*self.info['ImageWidth'],self.info['XResolution']),
+																							 np.arange(0,self.info['YResolution']*self.info['ImageLength'],self.info['YResolution']))
+
+		########################################################
+	  ### 3. GET A MASK OF NON-TISSUE
 
 ###############################################################################
 class Counter:
@@ -125,13 +213,49 @@ class Counter:
 	def __init__(self, image, **kwargs):
 
 		# parse the kwargs
-		self.sigma = kwargs.get('sigma',5)	
+		self.sigma   = kwargs.get('sigma',5)	
+		self.arealim = kwargs.get('arealim',[200,800])	
+		self.athresh = kwargs.get('athresh',1)
 
 		# store the array of the image
-		self.image = image.array
+		self.image  = image.array
+		self.x      = image.x
+		self.y      = image.y
+		self.res    = image.info['XResolution']
 
 		# smooth the image
-		self.smooth = ndimage.gaussian_filter(self.array,self.sigma)
+		self.smooth = ndimage.gaussian_filter(self.image,self.sigma)
 
-		# find the peaks in the smoothed image
-		self.peaks = 
+		# get a threshold for the image to get rid of background pixels and then identify cellular
+		# tissue
+		self.bkgrT = mahotas.thresholding.otsu(self.smooth,ignore_zeros=False)
+		self.cellT = mahotas.thresholding.otsu(self.smooth*(self.smooth > self.bkgrT),ignore_zeros=True)
+
+		# find the peaks in the smoothed image to get the potential cells
+		#self.peaks = pymorph.regmax(self.smooth > np.max(self.bkgrT,self.cellT*self.athresh))
+		self.peaks = pymorph.regmax(self.smooth > self.cellT*self.athresh)
+		[labels,n] = ndimage.label(self.peaks)
+
+		# get the distance transform
+		#dist = ndimage.distance_transform_edt(self.smooth > np.max(self.bkgrT,self.cellT*self.athresh))
+		dist = ndimage.distance_transform_edt(self.smooth > self.cellT*self.athresh)
+		dist = dist.max() - dist
+		dist -= dist.min()
+		dist = dist/float(dist.ptp()) * 255
+		dist = dist.astype(np.uint8)
+		self.dist = dist
+
+		# determine the region of each putative cell and its area
+		cells = pymorph.cwatershed(dist,labels)
+		areas = pymorph.blob(cells,'area',output='image')
+
+		# eliminate cells outside of the area limits
+		cells = cells * np.bitwise_and(areas >= self.arealim[0],areas <= self.arealim[1])
+
+		# re-label the cells, count them, get their areas, centres, and bounding rectangles
+		[self.cells, self.n] = ndimage.label(cells > 0)
+		self.area   = pymorph.blob(self.cells,'area',output='data') * (self.res ** 2)
+		self.centre = pymorph.blob(self.cells,'centroid',output='data')
+		self.box    = pymorph.blob(self.cells,'boundingbox',output='data')
+
+		# get the image intensity for each cell
